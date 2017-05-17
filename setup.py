19eba59318fa9000c0ba61ab349b40b581fd7e79
Nicolas Wavrant <nicolas.wavrant@gmail.com>
2017-05-17 13:37:15 +0200

    improves setup.py and allow running standalone tests
    (Standalone tests can be run with "python setup.py test")

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -11,6 +11,7 @@ def read(fname):
 setup(
     name = "django-feed-manager",
     version = "0.1",
+    packages=find_packages(),
     author = "Nicolas Wavrant",
     author_email = "nicolas.wavrant@gmail.com",
     description = ("A feed manager for Django, aiming to report automated tasks results in a standard format."),
@@ -26,9 +27,20 @@ setup(
             'reporter=cli.reporter:main',
         ],
     },
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+    ),
     classifiers = [
+        "Environment :: Web Environment",
+        "Framework :: Django",
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
 )
---
+++ test_settings.py
@@ -0,0 +1,19 @@
+#DATABASE_ENGINE = 'sqlite3'
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('.', 'db.sqlite3'),
+    }
+}
+
+ROOT_URLCONF = 'feedmanager.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.contenttypes',
+    'feedmanager',
+)
